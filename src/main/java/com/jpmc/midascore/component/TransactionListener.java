174300b3015d9f1ea953a35bbe91720d13a493b7
omkar dubal <omkar.b.dubal@gmail.com>
2026-03-07 19:23:13 +0530

    add transaction validation and database recording

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,6 +1,10 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRepository;
+import com.jpmc.midascore.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -11,8 +15,33 @@ public class TransactionListener {
 
     private static final Logger logger = LoggerFactory.getLogger(TransactionListener.class);
 
+    private final UserRepository userRepository;
+    private final TransactionRepository transactionRepository;
+
+    public TransactionListener(UserRepository userRepository, TransactionRepository transactionRepository) {
+        this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
+    }
+
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-group")
     public void listen(Transaction transaction) {
-        logger.info("Received transaction: {}", transaction);
+        UserRecord sender = userRepository.findById(transaction.getSenderId());
+        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
+
+        if (sender == null || recipient == null) return;
+        if (sender.getBalance() < transaction.getAmount()) return;
+
+        sender.setBalance(sender.getBalance() - transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
+        userRepository.save(sender);
+        userRepository.save(recipient);
+        transactionRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount()));
+
+        logger.info("Processed transaction: {} -> {}, amount: {}", sender.getName(), recipient.getName(), transaction.getAmount());
+
+        userRepository.findAll().forEach(user ->
+                logger.info("User balance: {} = {}", user.getName(), user.getBalance())
+        );
     }
 }
