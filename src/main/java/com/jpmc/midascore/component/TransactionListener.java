57c8b12240d6c4618a18d8ce3d418974ce1f4e4d
omkar dubal <omkar.b.dubal@gmail.com>
2026-03-08 11:07:02 +0530

    add TransactionRepository for persisting transaction records

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -2,6 +2,7 @@ package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
@@ -9,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class TransactionListener {
@@ -17,10 +19,12 @@ public class TransactionListener {
 
     private final UserRepository userRepository;
     private final TransactionRepository transactionRepository;
+    private final RestTemplate restTemplate;
 
-    public TransactionListener(UserRepository userRepository, TransactionRepository transactionRepository) {
+    public TransactionListener(UserRepository userRepository, TransactionRepository transactionRepository, RestTemplate restTemplate) {
         this.userRepository = userRepository;
         this.transactionRepository = transactionRepository;
+        this.restTemplate = restTemplate;
     }
 
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-group")
@@ -31,14 +35,18 @@ public class TransactionListener {
         if (sender == null || recipient == null) return;
         if (sender.getBalance() < transaction.getAmount()) return;
 
+        Incentive incentive = restTemplate.postForObject(
+                "http://localhost:8080/incentive", transaction, Incentive.class);
+        float incentiveAmount = incentive != null ? incentive.getAmount() : 0;
+
         sender.setBalance(sender.getBalance() - transaction.getAmount());
-        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
 
         userRepository.save(sender);
         userRepository.save(recipient);
-        transactionRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount()));
+        transactionRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount(), incentiveAmount));
 
-        logger.info("Processed transaction: {} -> {}, amount: {}", sender.getName(), recipient.getName(), transaction.getAmount());
+        logger.info("Processed transaction: {} -> {}, amount: {}, incentive: {}", sender.getName(), recipient.getName(), transaction.getAmount(), incentiveAmount);
 
         userRepository.findAll().forEach(user ->
                 logger.info("User balance: {} = {}", user.getName(), user.getBalance())
